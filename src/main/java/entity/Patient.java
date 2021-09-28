2db317a82e6664092f3a095d7e3010b14793d5f6
MohammadReza8080 <emohammadreza568@gmil.com>
2021-09-28 18:54:19 +0330

    Initial patient entity.

---
+++ src/main/java/entity/Patient.java
@@ -0,0 +1,69 @@
+package entity;
+
+
+import java.sql.Date;
+
+public class Patient {
+    private int id;
+    private String name;
+    private int age;
+    private Sex sex;
+    private Date birthdate;
+    private String address;
+
+    public Patient(String name, int age, Sex sex, Date birthdate, String address) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.birthdate = birthdate;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/entity/Sex.java
@@ -0,0 +1,11 @@
+package entity;
+
+public enum Sex {
+    MALE ("male"),FEMALE ("female");
+
+    private String sex;
+
+    Sex(String sex) {
+        this.sex = sex;
+    }
+}
